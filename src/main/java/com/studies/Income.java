266a99cd8d0987961f64c589ad37eb82b7546a51
Lauro Gomes Martins Ouverney <laurogmouverney2@gmail.com>
2025-09-07 14:49:04 -0300

    Add Income Class

---
+++ src/main/java/com/studies/Income.java
@@ -0,0 +1,16 @@
+package com.studies;
+
+public class Income extends Transaction{
+
+    public Income(String description, double valor, String date) {
+        super(description, valor, date);
+    }
+
+    public Income(String description, double valor) {
+        super(description, valor);
+    }
+
+    public Income(double valor) {
+        super(valor);
+    }
+}

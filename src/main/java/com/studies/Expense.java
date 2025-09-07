8a0dc4a68ed25e52c7669947023d87afbd1b521b
Lauro Gomes Martins Ouverney <laurogmouverney2@gmail.com>
2025-09-07 13:00:51 -0300

    Add Expense Class

---
+++ src/main/java/com/studies/Expense.java
@@ -0,0 +1,27 @@
+import com.studies.Transaction;
+
+public class Expense extends Transaction {
+
+    private String category;
+
+    public Expense(String description, double valor, String date) {
+        super(description, valor, date);
+    }
+
+    public Expense(String description, double valor) {
+        super(description, valor);
+    }
+
+    public Expense(double valor) {
+        super(valor);
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+}
